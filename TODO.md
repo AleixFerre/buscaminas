18904b2f70024cc90975040211ec1f986b062e1e
Aleix Ferré <thecatalahd@gmail.com>
2023-01-30 00:18:22 +0100

    Click funcionality

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# TODO
+
+- Funcionalitat de gameplay
+  - Perdre al clicar una bomba
+  - Poder reinicar la partida
+  - Que no puguis clicar una bomba la primera vegada
+  - Que puguis marcar bomba (bandera) amb el botó dret
--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+const MATRIX_SIZE = 10;
+
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
   matrix: number[][] = [];
-  canShow: boolean[][] = [];
+  clicked: boolean[][] = [];
 
   ngOnInit(): void {
     this.createMatrix();
@@ -16,27 +18,27 @@ export class BoardComponent implements OnInit {
   }
 
   private createMatrix() {
-    for (let i: number = 0; i < 10; i++) {
+    for (let i: number = 0; i < MATRIX_SIZE; i++) {
       this.matrix[i] = [];
-      this.canShow[i] = [];
-      for (let j: number = 0; j < 10; j++) {
+      this.clicked[i] = [];
+      for (let j: number = 0; j < MATRIX_SIZE; j++) {
         this.matrix[i][j] = 0;
-        this.canShow[i][j] = false;
+        this.clicked[i][j] = false;
       }
     }
   }
 
   private placeBombs() {
-    for (let i: number = 0; i < 10; i++) {
-      let x = Math.floor(Math.random() * 10);
-      let y = Math.floor(Math.random() * 10);
+    for (let i: number = 0; i < MATRIX_SIZE; i++) {
+      let x = Math.floor(Math.random() * MATRIX_SIZE);
+      let y = Math.floor(Math.random() * MATRIX_SIZE);
       this.matrix[x][y] = -1;
     }
   }
 
   private calculateNumbers() {
-    for (let i: number = 0; i < 10; i++) {
-      for (let j: number = 0; j < 10; j++) {
+    for (let i: number = 0; i < MATRIX_SIZE; i++) {
+      for (let j: number = 0; j < MATRIX_SIZE; j++) {
         if (this.matrix[i][j] !== -1) {
           this.matrix[i][j] = this.calculateCircundant(i, j);
         }
@@ -60,10 +62,27 @@ export class BoardComponent implements OnInit {
 
   private calculateLimits(x: number, y: number) {
     const limits = [x - 1, x + 1, y - 1, y + 1];
-    return limits.map((n) => Math.min(Math.max(n, 0), this.matrix.length - 1));
+    return limits.map((n) => Math.min(Math.max(n, 0), MATRIX_SIZE - 1));
   }
 
-  public clickPos(i:number, j:number) {
-    this.canShow[i][j] = true;
+  public clickPos(x: number, y: number) {
+    if (this.clicked[x][y]) {
+      return;
+    }
+
+    this.clicked[x][y] = true;
+
+
+    if (this.matrix[x][y] !== 0) {
+      return;
+    }
+
+    const [startX, endX, startY, endY] = this.calculateLimits(x, y);
+
+    for (let i = startX; i <= endX; i++) {
+      for (let j = startY; j <= endY; j++) {
+        this.clickPos(i, j);
+      }
+    }
   }
 }
